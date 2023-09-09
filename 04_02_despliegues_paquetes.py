71907270bb11ca0931e3dd616d1b801d2fa01ea1
Cristhian-L-Q-N <tecn0crisis0@gmail.com>
2023-09-08 22:53:10 -0300

    script about packages

---
+++ 04_02_despliegues_paquetes.py
@@ -0,0 +1,8 @@
+#una vez tengamos el modulo principal 
+#Para ejecutar el script principal main.py, abre una terminal en la carpeta mi_proyecto y escribe:
+
+
+#python main.py
+
+# Deberías ver la salida en la consola que muestra los
+#  resultados de las funciones y clases importadas desde los módulos.
